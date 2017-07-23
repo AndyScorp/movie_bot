329bcc03307663fe6f7796781dfe917f1e834f8f
rrlero <roman@xlan.net.ua>
2017-07-23 17:52:14 +0300

    bot added

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
-const token = require('/home/rrlero/WebstormProjects/ChanBot/token_telegram.json').token;
+
 
 module.exports = {
     telegram: {
-        token: process.env.TELEGRAM_TOKEN || token,
+        token: process.env.TELEGRAM_TOKEN || require('token_telegram.json').token,
         externalUrl: process.env.CUSTOM_ENV_VARIABLE || 'https://movie-lite-bot.herokuapp.com',
         port: process.env.PORT || 443,
         host: '0.0.0.0'
