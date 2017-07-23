f4af60ef996d9f1461298450fb84e45bdb50e6ef
rrlero <roman@xlan.net.ua>
2017-07-23 17:58:39 +0300

    bot added

--- src/lib/bot.js
+++ src/lib/bot.js
@@ -5,7 +5,7 @@ const config = require("../config");
 module.exports = function Constructor() {
     if (process.env.NODE_ENV === 'production') {
 
-        this.bot = new TelegramBot(token, { webHook: { port: config.telegram.port, host: config.telegram.host } });
+        this.bot = new TelegramBot(config.telegram.token, { webHook: { port: config.telegram.port, host: config.telegram.host } });
         this.bot.setWebHook(config.telegram.externalUrl + ':443/bot' + config.telegram.token);
 
     } else {
