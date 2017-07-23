7dc945a26eaa7d2265eed7611e023664999c6578
rrlero <roman@xlan.net.ua>
2017-07-23 17:01:21 +0300

    bot added

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,8 @@
+token = require('token_telegram.json')["token"];
+
 module.exports = {
     telegram: {
-        token: process.env.TELEGRAM_TOKEN || '409804897:AAHW-Fx6Ri8hkMrA9G_4cwd8wWM3mtAwa-c',
+        token: process.env.TELEGRAM_TOKEN || token,
         externalUrl: process.env.CUSTOM_ENV_VARIABLE || 'https://movie-lite-bot.herokuapp.com',
         port: process.env.PORT || 443,
         host: '0.0.0.0'
