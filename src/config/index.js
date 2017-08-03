25217b465ea25a4a3b8940bbbea7075dc5ac064b
rrlero <roman@xlan.net.ua>
2017-08-04 00:08:28 +0300

    resolved mistakes with DB

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     telegram: {
         token: process.env.TELEGRAM_TOKEN || require('../token_telegram.json').token,
         externalUrl: process.env.CUSTOM_ENV_VARIABLE || 'https://movie-lite-bot.herokuapp.com',
-        port: 8080 || 8080,
+        port: 8443 || 8443,
         host: process.env.HOST || '0.0.0.0'
     }
 };
