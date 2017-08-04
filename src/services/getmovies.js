184578db81862348ee6e7fe838ec22195597561f
rrlero <roman@xlan.net.ua>
2017-08-04 22:53:19 +0300

    resolved mistakes with DB

--- src/services/getmovies.js
+++ src/services/getmovies.js
@@ -14,7 +14,9 @@ module.exports.getMovies = function (url, element) {
                   var a = $(this);
                   var index = a.children()['1'].attribs.href.replace(/Movie.aspx\?id=/g, '');
                   var mov = (a.text().trim().replace(/\t/g, '').replace(/\n\n/g, '\n') + '\n' + index).split('\n');
-                  array.push(mov)
+                  if (isInArray(array, mov[0])) {
+                      array.push(mov)
+                  }
               });
               return resolve(array)
           } else {
@@ -22,4 +24,17 @@ module.exports.getMovies = function (url, element) {
           }
       });
   })
+};
+
+var isInArray = function (array, mov) {
+    if (!array.length) {
+        return true
+    } else {
+        for (var j=0; j<array.length; j++) {
+            if (array[j][0] === mov) {
+                return false
+            }
+        }
+    }
+    return true
 };
